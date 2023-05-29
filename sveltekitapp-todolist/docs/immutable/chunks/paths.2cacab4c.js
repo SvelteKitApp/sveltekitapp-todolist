eb2a845a2ca1c2b82f0f73a9ec95e22a89a60946
Gerzhan <nikolay.gerzhan@gmail.com>
2023-05-29 09:26:41 +0700

    Updates

--- sveltekitapp-todolist/docs/immutable/chunks/paths.2cacab4c.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1ua7cfy)==null?void 0:s.base)??"/sveltekitapp-todolist";var t;const e=((t=globalThis.__sveltekit_1ua7cfy)==null?void 0:t.assets)??a;export{e as a,a as b};
