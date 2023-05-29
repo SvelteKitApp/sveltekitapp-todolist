5b94cf77c29e01eb9ff9b8e8631fe202a856595d
Gerzhan <nikolay.gerzhan@gmail.com>
2023-05-29 09:34:20 +0700

    Updates

---
+++ docs/immutable/chunks/paths.df2f2e25.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_p2xf3m)==null?void 0:s.base)??"/sveltekitapp-todolist";var t;const a=((t=globalThis.__sveltekit_p2xf3m)==null?void 0:t.assets)??e;export{a,e as b};
